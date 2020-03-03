fbc5e32193fd2765f234644e0f76f162862f28e3
hgeraso <hgeraso@unicauca.edu.co>
2020-03-03 12:09:44 -0500

    colecciones de estudiante y curso cargadas con sus cursos y estudiantes respectivos

--- server/controllers/load.controller.js
+++ server/controllers/load.controller.js
@@ -31,7 +31,7 @@ const loadCtrl={}
 //   // --------------------------Fin de la manera de trabajar a partir de promesas--------------------
 }
 loadCtrl.loadCollections = async  (req,res) => {
-   // Inicio Seccion para hacer las consultas avanzadas y guardar el resultado de la promesa en los diferentes arreglos-----
+ // ############Inicio Seccion para hacer las consultas avanzadas y guardar el resultado de la promesa en los diferentes arreglos-----
     const courses= await segui.distinct( "course"); //devuelve lista total de cursos unicos
     const students= await segui.distinct( "username");// devuelve lista total de estudiantes unicos
     const studentsOfCourse=await segui.aggregate([      //devulve lista completa de cursos con sus respectivos estudiantes.
@@ -40,29 +40,47 @@ loadCtrl.loadCollections = async  (req,res) => {
     const coursesOfStudents=await segui.aggregate([
         { $group: { _id  : "$username", courses : {$addToSet: "$course"}  } }
     ])
-    // Inicio Seccion para hacer las consultas avanzadas y guardar el resultado de la promesa en los diferentes arreglos-----
+// ###########fin Seccion para hacer las consultas avanzadas y guardar el resultado de la promesa en los diferentes arreglos-----
 
-    // ############Inicio Seccion Para guardar los cursos a la coleccion y asignarles un Id-##############  
-    courses.forEach(course=> {
+{ // ############Inicio Seccion Para guardar los cursos a la coleccion y asignarles un Id-##############  
+   /* courses.forEach(course=> {
         const reqCourse={
             "name" : course,
             "students":[]
         };
         console.log('viendo tipo de funcion guardar curso:',courseCtrl.createCourse(reqCourse).then(v=>{console.log('new course saved',v)}).catch(error=>{console.log('el errorrr es:',error);}))
-    });
+    });*/
     // ----- Fin Seccion Para guardar todos los  cursos consultados a la coleccion y asignarles un Id-------  
 
     //#### Inicio seccion para guardar los estudiantes consultados a la coleccion y asignales un Id############
-    students.forEach(student => {
+    /*students.forEach(student => {
         const reqStudent= {
               "name":student,
               "courses":[]
         }
         console.log("viendo tipo de funcion guardar estudiante",studenttCtrl.createStudentt(reqStudent).then(v=>{console.log('new student saved:',v)}).catch(error=>{console.log('El error de estudiante es:',v)}));
         
-    });
+    });*/
+} //#### fin seccion para guardar los estudiantes consultados a la coleccion y asignales un Id############
 
-    res.json ('data Loaded');
+    studentsOfCourse.forEach(register => {
+        
+            const reqCourse={
+                "name" : register._id,
+                "students":register.usernames
+            };
+            console.log('viendo tipo de funcion guardar curso:',courseCtrl.createCourse(reqCourse).then(v=>{console.log('new course saved',v)}).catch(error=>{console.log('el errorrr es:',error);}))
+        });
+    
+        coursesOfStudents.forEach(register => {
+        
+            const reqStudent={
+                "name" : register._id,
+                "students":register.courses
+            };
+            console.log('viendo tipo de funcion guardar estudiante:',studenttCtrl.createStudentt(reqStudent).then(v=>{console.log('new student saved',v)}).catch(error=>{console.log('el errorrr es:',error);}))
+        });
+    res.json (studentsOfCourse);
     
 }
 
