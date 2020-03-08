d7d3b765048d8f99a7b2c37c5ad7818ce97c0e6b
aldairl <aldairl@unicauca.edu.co>
2020-03-07 23:53:18 -0500

    network with vis success

--- frontend/src/app/components/estadisticos/estadisticos.component.ts
+++ frontend/src/app/components/estadisticos/estadisticos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Network, DataSet, Node, Edge, IdType } from 'vis';
 
 @Component({
   selector: 'app-estadisticos',
@@ -6,10 +7,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./estadisticos.component.css']
 })
 export class EstadisticosComponent implements OnInit {
+  public nodes: Node;
+  public edges: Edge;
+  public network: Network;
 
   constructor() { }
 
   ngOnInit(): void {
+  
+    this.createNetwork();
+
+  }
+
+  createNetwork() {
+    var nodes = new DataSet([
+      { id: 1, label: 'Node 1' },
+      { id: 2, label: 'Node 2' },
+      { id: 3, label: 'Node 3' },
+      { id: 4, label: 'Node 4' },
+      { id: 5, label: 'Node 5' }
+    ]);
+    // create an array with edges
+    var edges = new DataSet([
+      { from: 1, to: 3 },
+      { from: 1, to: 2 },
+      { from: 2, to: 4 },
+      { from: 2, to: 5 },
+      { from: 3, to: 3 }
+    ]);
+
+    // create a network
+    var container = document.getElementById('mynetwork');
+    var data = {
+      nodes: nodes,
+      edges: edges
+    };
+    var options = {};
+    var network = new Network(container, data, options);
+
   }
 
 }
