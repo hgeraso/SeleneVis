8a7d1e748d7fc3c6b59e2126feef6fa5cc0d52e6
aldairl <aldairl@unicauca.edu.co>
2020-03-22 16:11:21 -0500

    initializing

--- server/controllers/seguimiento.controller.js
+++ server/controllers/seguimiento.controller.js
@@ -1,29 +1,30 @@
 const Seguimiento = require('../models/seguimiento');
 
-const seguimientoCtrl={}; //he definido un objeto para luego aplicar metodos.
-seguimientoCtrl.getDocs = async(req,res)=>{
-    
-    const docSeguimientoConsulta= await Seguimiento.find();
+const seguimientoCtrl = {}; //he definido un objeto para luego aplicar metodos.
+seguimientoCtrl.getDocs = async (req, res) => {
+
+    const docSeguimientoConsulta = await Seguimiento.find();
     res.json(docSeguimientoConsulta);
 };
-seguimientoCtrl.getStudentsCourse = async(req,res)=>{
-    const students= await Seguimiento.aggregate([
-        { $group: { _id  : "$course", usernames : {$addToSet: "$username"}  } }
+seguimientoCtrl.getStudentsCourse = async (req, res) => {
+    const students = await Seguimiento.aggregate([
+        { $group: { _id: "$course", usernames: { $addToSet: "$username" } } }
     ])
-        res.json(students); 
+    res.json(students);
 }
 
-seguimientoCtrl.getStudents = async(req,res)=>{
-    const students= await Seguimiento.distinct( "username")
-        res.json(students); 
+seguimientoCtrl.getStudents = async (req, res) => {
+    const students = await Seguimiento.distinct("username")
+    res.json(students);
 }
-seguimientoCtrl.getCourses = async(req,res)=>{
-    const courses= await Seguimiento.distinct( "course"); 
-        return courses; //Este return es muy importante puesto que se convierte en lo que retorna y hace posible que se use desde 
-                         //otra parte de la aplicacion. el res.send() renderiza en el browser la informacion pero a la larga no esta retornando nada en l
-                            //la funcion.
-        
+seguimientoCtrl.getCourses = async (req, res) => {
+    const courses = await Seguimiento.distinct("course");
+    // return courses; //Este return es muy importante puesto que se convierte en lo que retorna y hace posible que se use desde 
+    //otra parte de la aplicacion. el res.send() renderiza en el browser la informacion pero a la larga no esta retornando nada en l
+    //la funcion.
+    res.json(courses);
+
 }
 
 
-module.exports=seguimientoCtrl;
+module.exports = seguimientoCtrl;
