d176abf9e5b2b652a806c3871a3ea0852536b9e9
hgeraso <hgeraso@unicauca.edu.co>
2020-04-09 06:00:52 -0500

    funcionando tiempo de examen sin muchas validaciones

--- server/controllers/statistics.controller.js
+++ server/controllers/statistics.controller.js
@@ -15,10 +15,10 @@ statisticCtrl.getStatistics = async(req,res)=>{
     const numForos=await seguimiento.find( { $and : [
         { $or : [ { name : "edx.forum.comment.created" }, { name : "edx.forum.response.created" },{ name : "edx.forum.thread.created" }] },
         {  "username":/*"Gustavo_Ramirez_Staff"*/"Helberth_Medina_Sandoval","course":"Unicauca+LeanStartUp+2019-II"}
-    ]}).count()
+    ]}).count();
 
     const numExamenes= await seguimiento.find( { $and : [
-        { $or : [ { name : "problem_check" }, { name : "problem_graded" }] },
+        { $or : [ { name : "problem_check" },/*  { name : "problem_graded" } */] },
         {  "username":"Gustavo_Ramirez_Staff","course":"Unicauca+Intro_IoT+2019-II"}
     ]}).count();
     
@@ -60,11 +60,8 @@ statisticCtrl.getStatistics = async(req,res)=>{
     ]
     );
     const timeVideo= await statisticCtrl.getTimeVideo();
-    console.log(timeVideo);
-   /*  .then(v=>{console.log("el valor de tiempo de video es",v); return v;}).catch(error=>{console.log('el errorrr es:',error)});
-    console.log("el valor de tiempo de video es",timeVideo); */
-
-    res.json(
+    const timeExam= await statisticCtrl.getTimeExam();
+ res.json(
         {"numVideos":numVideos,
          "numContenido":numContenido,
          "numForos":numForos,
@@ -72,9 +69,11 @@ statisticCtrl.getStatistics = async(req,res)=>{
          "numSesiones":numSesiones,
          "numVideosDiferentes":numVideosDiferentes[0].numVideosDifferents,
          "numSesionesDiferentes":numSesionesDiferentes[0].numsesionesDifferents,
-         "TimeVideo": timeVideo
+         "TimeVideo": timeVideo,
+         "TimeExam":timeExam
         }
-    );
+    ); 
+    
 }
     statisticCtrl.getTimeVideo = async function(){
 
@@ -108,6 +107,43 @@ statisticCtrl.getStatistics = async(req,res)=>{
          console.log("es de tipo :",typeof(bdTimeVideo.length),bdTimeVideo.length);
          //res.json({"la suma total es": sumTime}); //tiempo en segundo de interaccion directa con el video
          //res.json(bdTimeVideo);
+         return sumTime;
+    }
+    
+    statisticCtrl.getTimeExam= async(req,res)=>{
+
+       
+        const bdTimeExam= await seguimiento.find({ $and : [
+         {"username":"Karold_Ordonez_Ceron","course":"Unicauca+LeanStartUp+2019-II"},  
+         ]}).sort("date").sort("time");
+         sumTime=0; // en segundos 
+         for (let i = 0; i < bdTimeExam.length-1; i++) {
+             //console.log(i+1);
+             console.log("entro al ciclo ",i);
+             if (bdTimeExam[i].toObject().date==bdTimeExam[i+1].toObject().date){
+                    if(bdTimeExam[i].toObject().name=="problem_check"){
+                        console.log("entro al Time exam");
+                        segTimeEnd= (bdTimeExam[i].toObject().time.substr(0,2)*3600)+bdTimeExam[i].toObject().time.substr(3,2)*60 + (bdTimeExam[i].toObject().time.substr(6,2)*1);     
+                        segTimeInit= (bdTimeExam[i-1].toObject().time.substr(0,2)*3600)+bdTimeExam[i-1].toObject().time.substr(3,2)*60 + (bdTimeExam[i-1].toObject().time.substr(6,2)*1);
+                        resTime=segTimeEnd-segTimeInit;
+                        console.log("el residuo es:",resTime);
+                            
+                        
+                            if(bdTimeExam[i-1].toObject().name=="Signin"){
+                                console.log("se descarta por Signin");
+    
+                            }else{
+                                sumTime=sumTime+resTime;
+                                console.log("la suma parcial de Time exam es:",sumTime);
+                            }
+                        
+                    }
+            }    
+         } 
+         console.log("es de tipo :",typeof(bdTimeExam.length),bdTimeExam.length);
+         console.log("la suma total de TimeExam es:",sumTime);
+         //res.json({"la suma total es": sumTime}); //tiempo en segundo de interaccion directa con el video
+         //res.json({ "time Exam":sumTime});
          return sumTime;
     }
     
--- server/routes/prueba.routes.js
+++ server/routes/prueba.routes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router =express.Router();
 const pruebaCtrl = require('../controllers/prueba.controller');
 router.get('/time', pruebaCtrl.getTime2);
+router.get('/timeExam', pruebaCtrl.getTimeExam);
+
 
 
 
