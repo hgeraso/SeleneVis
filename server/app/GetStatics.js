64091e111d1a8c0fa0a6dc6a099b65e025451fd4
hgeraso <hgeraso@unicauca.edu.co>
2020-06-26 09:27:02 -0500

    funciones TimeExam y TimeOthers Corregidas de valor negativo

--- server/app/GetStatics.js
+++ server/app/GetStatics.js
@@ -231,49 +231,96 @@ statisticCtrl.getTimeOthers = async (course, student) => {
             { "username": student, "course": course },
         ]
     }).sort("date").sort("time");
-    sumTime = 0; // en segundos 
-    for (let i = 0; i < bdTimeOthers.length - 1; i++) {
-        //console.log(i+1);
-        // console.log("entro al ciclo ", i);
-        if ([[bdTimeOthers[i + 1].toObject().date.substr(5, 2) * 30 * 24 * 60 * 60 + bdTimeOthers[i + 1].toObject().date.substr(8, 2) * 24 * 60 * 60]
-            - [bdTimeOthers[i].toObject().date.substr(5, 2) * 30 * 24 * 60 * 60 + bdTimeOthers[i].toObject().date.substr(8, 2) * 24 * 60 * 60]] <= (3600 * 24)) {
-            if (bdTimeOthers[i + 1].toObject().name == "Signin") {
-                // console.log("el siguiente evento es  signin");
-                // console.log("aplica el tiempo de otros");
-                segTimeInit = (bdTimeOthers[i].toObject().time.substr(0, 2) * 3600) + bdTimeOthers[i].toObject().time.substr(3, 2) * 60 + (bdTimeOthers[i].toObject().time.substr(6, 2) * 1);
-                segTimeEnd = (bdTimeOthers[i + 1].toObject().time.substr(0, 2) * 3600) + bdTimeOthers[i + 1].toObject().time.substr(3, 2) * 60 + (bdTimeOthers[i + 1].toObject().time.substr(6, 2) * 1);
-                resTime = segTimeEnd - segTimeInit;
-                // console.log("el residuo es:", resTime);
-                sumTime = sumTime + resTime;
-                // console.log("sumTime sumado por signin tiene el valor parcial de  ", sumTime);
-            }
-            else if (bdTimeOthers[i].toObject().name == "pause_video") {
-                // console.log("ingreso por pause video")
-                segTimeInit = (bdTimeOthers[i].toObject().time.substr(0, 2) * 3600) + bdTimeOthers[i].toObject().time.substr(3, 2) * 60 + (bdTimeOthers[i].toObject().time.substr(6, 2) * 1);
-                segTimeEnd = (bdTimeOthers[i + 1].toObject().time.substr(0, 2) * 3600) + bdTimeOthers[i + 1].toObject().time.substr(3, 2) * 60 + (bdTimeOthers[i + 1].toObject().time.substr(6, 2) * 1);
-                resTime = segTimeEnd - segTimeInit;
-                // console.log("el residuo es:", resTime);
-                sumTime = sumTime + resTime;
-                // console.log("el sumTime sumado por pause video tiene el valor parcial de ", sumTime)
+    sumTime=0; // en segundos 
+    for (let i = 0; i < bdTimeOthers.length-1; i++) {
+    //console.log(i+1);
+    console.log("entro al ciclo ",i);
+    if(bdTimeOthers[i+1].toObject().date == bdTimeOthers[i].toObject().date)
+    {
+        console.log("entro al ciclo de fechas igules");
+       if(bdTimeOthers[i+1].toObject().name=="Signin"){
+           console.log("el siguiente evento es  signin");
+           console.log("aplica el tiempo de otros");
+           console.log("la fecha incial es ", bdTimeOthers[i].toObject().date);
+           console.log("la fecha final es",bdTimeOthers[i+1].toObject().date);
+           console.log("la hora inicial es",bdTimeOthers[i].toObject().time);
+           console.log("la hora final es ", bdTimeOthers[i+1].toObject().time  )
+           segTimeInit= (bdTimeOthers[i].toObject().time.substr(0,2)*3600)+bdTimeOthers[i].toObject().time.substr(3,2)*60 + (bdTimeOthers[i].toObject().time.substr(6,2)*1);     
+           segTimeEnd= (bdTimeOthers[i+1].toObject().time.substr(0,2)*3600)+bdTimeOthers[i+1].toObject().time.substr(3,2)*60 + (bdTimeOthers[i+1].toObject().time.substr(6,2)*1);
+           resTime=segTimeEnd-segTimeInit;
+           console.log("el residuo es:",resTime);
+           sumTime=sumTime + resTime;
+           console.log("sumTime sumado por signin tiene el valor parcial de  ", sumTime);
+       }
+       else if(bdTimeOthers[i].toObject().name=="pause_video"){
+           console.log("ingreso por pause video")
+           segTimeInit= (bdTimeOthers[i].toObject().time.substr(0,2)*3600)+bdTimeOthers[i].toObject().time.substr(3,2)*60 + (bdTimeOthers[i].toObject().time.substr(6,2)*1);     
+           segTimeEnd= (bdTimeOthers[i+1].toObject().time.substr(0,2)*3600)+bdTimeOthers[i+1].toObject().time.substr(3,2)*60 + (bdTimeOthers[i+1].toObject().time.substr(6,2)*1);
+           resTime=segTimeEnd-segTimeInit;
+           console.log("el residuo es:",resTime);
+           sumTime=sumTime + resTime; 
+           console.log("el sumTime sumado por pause video tiene el valor parcial de ", sumTime)
 
-            }
-            else if (bdTimeOthers[i].toObject().name == "play_video") {
-                // console.log("ingreso al play video")
-                segTimeInit = (bdTimeOthers[i].toObject().time.substr(0, 2) * 3600) + bdTimeOthers[i].toObject().time.substr(3, 2) * 60 + (bdTimeOthers[i].toObject().time.substr(6, 2) * 1);
-                segTimeEnd = (bdTimeOthers[i + 1].toObject().time.substr(0, 2) * 3600) + bdTimeOthers[i + 1].toObject().time.substr(3, 2) * 60 + (bdTimeOthers[i + 1].toObject().time.substr(6, 2) * 1);
-                resTime = segTimeEnd - segTimeInit;
-                // console.log("el residuo es:", resTime);
-                if (resTime > 60 * 7) {
-                    sumTime = sumTime + resTime;
-                    // console.log("el sumTime sumado por play video mayor a 7 min tiene el valor parcial de ", sumTime)
+       }
+      else if(bdTimeOthers[i].toObject().name=="play_video"){
+           console.log("ingreso al play video")
+           segTimeInit= (bdTimeOthers[i].toObject().time.substr(0,2)*3600)+bdTimeOthers[i].toObject().time.substr(3,2)*60 + (bdTimeOthers[i].toObject().time.substr(6,2)*1);     
+           segTimeEnd= (bdTimeOthers[i+1].toObject().time.substr(0,2)*3600)+bdTimeOthers[i+1].toObject().time.substr(3,2)*60 + (bdTimeOthers[i+1].toObject().time.substr(6,2)*1);
+           resTime=segTimeEnd-segTimeInit;
+           console.log("el residuo es:",resTime);
+           if (resTime > 60*7){
+               sumTime=sumTime + resTime;
+               console.log("el sumTime sumado por play video mayor a 7 min tiene el valor parcial de ",sumTime)
 
-                }
-            }
-        }
-    }
-    //res.json({"el valor de time others es: ":sumTime});
-    //res.json(bdTimeOthers);
-    return sumTime;
+           }
+       }
+
+    }else if([[bdTimeOthers[i+1].toObject().date.substr(5,2)*30*24*60*60 + bdTimeOthers[i+1].toObject().date.substr(8,2)*24*60*60 ]
+    -[bdTimeOthers[i].toObject().date.substr(5,2)*30*24*60*60 + bdTimeOthers[i].toObject().date.substr(8,2)*24*60*60]] <= (3600*24) ){
+               console.log("entro a la condiciòn de fechas diferentes pero menor a un dia");
+               if(bdTimeOthers[i+1].toObject().name=="Signin"){
+               console.log("el siguiente evento es  signin");
+               console.log("aplica el tiempo de otros");
+               console.log("la fecha incial es ", bdTimeOthers[i].toObject().date);
+               console.log("la fecha final es",bdTimeOthers[i+1].toObject().date);
+               console.log("la hora inicial es",bdTimeOthers[i].toObject().time);
+               console.log("la hora final es ", bdTimeOthers[i+1].toObject().time  )
+               segTimeInit= (bdTimeOthers[i].toObject().time.substr(0,2)*3600)+bdTimeOthers[i].toObject().time.substr(3,2)*60 + (bdTimeOthers[i].toObject().time.substr(6,2)*1);     
+               segTimeEnd= [parseInt(bdTimeOthers[i+1].toObject().time.substr(0,2))+24]*3600+bdTimeOthers[i+1].toObject().time.substr(3,2)*60 + (bdTimeOthers[i+1].toObject().time.substr(6,2)*1);
+               resTime=segTimeEnd-segTimeInit;
+               console.log("el residuo es:",resTime);
+               sumTime=sumTime + resTime;
+               console.log("sumTime sumado por signin tiene el valor parcial de  ", sumTime);
+           }
+           else if(bdTimeOthers[i].toObject().name=="pause_video"){
+               console.log("ingreso por pause video")
+               segTimeInit= (bdTimeOthers[i].toObject().time.substr(0,2)*3600)+bdTimeOthers[i].toObject().time.substr(3,2)*60 + (bdTimeOthers[i].toObject().time.substr(6,2)*1);     
+               segTimeEnd= [parseInt(bdTimeOthers[i+1].toObject().time.substr(0,2))+24]*3600+bdTimeOthers[i+1].toObject().time.substr(3,2)*60 + (bdTimeOthers[i+1].toObject().time.substr(6,2)*1);
+               resTime=segTimeEnd-segTimeInit;
+               console.log("el residuo es:",resTime);
+               sumTime=sumTime + resTime; 
+               console.log("el sumTime sumado por pause video tiene el valor parcial de ", sumTime)
+
+           }
+          else if(bdTimeOthers[i].toObject().name=="play_video"){
+               console.log("ingreso al play video")
+               segTimeInit= (bdTimeOthers[i].toObject().time.substr(0,2)*3600)+bdTimeOthers[i].toObject().time.substr(3,2)*60 + (bdTimeOthers[i].toObject().time.substr(6,2)*1);     
+               segTimeEnd= [parseInt(bdTimeOthers[i+1].toObject().time.substr(0,2))+24]*3600+bdTimeOthers[i+1].toObject().time.substr(3,2)*60 + (bdTimeOthers[i+1].toObject().time.substr(6,2)*1);
+               resTime=segTimeEnd-segTimeInit;
+               console.log("el residuo es:",resTime);
+               if (resTime > 60*7){
+                   sumTime=sumTime + resTime;
+                   console.log("el sumTime sumado por play video mayor a 7 min tiene el valor parcial de ",sumTime)
+
+               }
+           } 
+    }                 
+} 
+//res.json({"el valor de time others es: ":sumTime});
+// res.json(sumTime);
+return sumTime;
+
+
 
 }
 
