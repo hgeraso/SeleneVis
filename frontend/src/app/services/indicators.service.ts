ed27b4ed480b1581fc4a730b7aaa7e0e6b0981b3
aldairl <aldairl@unicauca.edu.co>
2020-06-30 20:04:15 -0500

    intent to graphics

--- frontend/src/app/services/indicators.service.ts
+++ frontend/src/app/services/indicators.service.ts
@@ -3,6 +3,7 @@ import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Indicator } from '../models/indicators';
 import { Observable } from 'rxjs';
+import { StadiscticGraph } from '../models/stadistics-graphs';
 
 @Injectable({
   providedIn: 'root'
@@ -16,5 +17,9 @@ export class IndicatorsService {
 
   getIndicatorsByCourse(course:string):Observable<Indicator[]>{
     return  this.http.post<Indicator[]>(this.URL_SERVER+'course', {course});
+  }
+
+  getStadisticsByCourse(course:string):Observable<StadiscticGraph[]>{
+    return this.http.post<StadiscticGraph[]>(this.URL_SERVER + 'course-stadistics', {course})
   }
 }
